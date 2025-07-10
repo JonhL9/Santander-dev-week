9aa0974bfebc3b52d3838b9c7975537bc31f4c27
Jonh <jlas@ic.ufal.br>
2025-07-10 20:09:15 -0300

    docs: add entities

---
+++ src/main/java/me/dio/santander_dev_week/Model/BaseItem.java
@@ -0,0 +1,40 @@
+package me.dio.santander_dev_week.Model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String icon;
+    private String descripton;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescripton() {
+        return descripton;
+    }
+
+    public void setDescripton(String descripton) {
+        this.descripton = descripton;
+    }
+}
---
+++ src/main/java/me/dio/santander_dev_week/Model/Feature.java
@@ -0,0 +1,11 @@
+package me.dio.santander_dev_week.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name="tb_feature")
+public class Feature extends  BaseItem{
+
+}
